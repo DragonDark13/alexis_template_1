a460d4be26af75937e54b3a106f88d3159eb5ef1
DragonDark13 <alekz.zak@mail.ru>
2017-06-10 18:37:29 +0300

    viewport cheker effect

--- javascript/script.js
+++ javascript/script.js
@@ -10,25 +10,34 @@ jQuery(document).ready(function($) {
             jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
         });
 
-        jQuery('.about .image_container, .text_container.left').viewportChecker({
+        jQuery('.about .image_container, .text_container.left, .top_post, .tool.left').viewportChecker({
 
               classToAdd: 'animated fadeInLeft',
-              repeat: true,
+              // repeat: true,
 
         });
 
-        jQuery('.about .text, .text_container.right').viewportChecker({
+        jQuery('.about .text, .text_container.right, .single_post, .tool.right').viewportChecker({
 
               classToAdd: 'animated fadeInRight',
-              repeat: true,
+              // repeat: true,
 
         });
 
-        jQuery('.service, .portfolio_container, .single_member_inner_container, .stat_container>*,.how_we_work .image_container, .single_package>*, .single_feature>*').viewportChecker({
+        jQuery('.service, .portfolio_container, .single_member_inner_container, .stat_container>*,.how_we_work .image_container, .single_package>*, .single_feature>*, .member_details').viewportChecker({
 
               classToAdd: 'animated zoomIn',
-              repeat: true,
+              // repeat: true,
 
         });
+
+        jQuery('.call , .content>.my_btn, .tool.bottom, .button_container').viewportChecker({
+
+              classToAdd: 'animated fadeInUp',
+              // repeat: true,
+        });
+
+
+
 	
 });
