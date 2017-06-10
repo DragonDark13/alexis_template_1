5cacdb9233f4b5bfbd6863a8e23a70337c11bfed
DragonDark13 <alekz.zak@mail.ru>
2017-06-10 17:27:11 +0300

    viewportchecker effect

--- javascript/script.js
+++ javascript/script.js
@@ -10,7 +10,24 @@ jQuery(document).ready(function($) {
             jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
         });
 
-        $('.dummy').viewportChecker({
+        jQuery('.about .image_container, .text_container.left').viewportChecker({
+
+              classToAdd: 'animated fadeInLeft',
+              repeat: true,
+
+        });
+
+        jQuery('.about .text, .text_container.right').viewportChecker({
+
+              classToAdd: 'animated fadeInRight',
+              repeat: true,
+
+        });
+
+        jQuery('.service, .portfolio_container, .single_member_inner_container, .stat_container>*,.how_we_work .image_container, .single_package>*, .single_feature>*').viewportChecker({
+
+              classToAdd: 'animated zoomIn',
+              repeat: true,
 
         });
 	
