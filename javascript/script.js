fff008d43126da149f708dfa6b134b8f6c83bd43
DragonDark13 <alekz.zak@mail.ru>
2017-06-30 16:11:44 +0300

    responsive end js effects

--- javascript/script.js
+++ javascript/script.js
@@ -1,43 +1,79 @@
 jQuery(document).ready(function($) {
 
-	var myWidth = 0;
-        myWidth = window.innerWidth;
-        jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
-        jQuery(window).resize(function(){
-            var myWidth = 0;
-            myWidth = window.innerWidth;
-            jQuery('#size').remove();
-            jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
-        });
+	// var myWidth = 0;
+ //        myWidth = window.innerWidth;
+ //        jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
+ //        jQuery(window).resize(function(){
+ //            var myWidth = 0;
+ //            myWidth = window.innerWidth;
+ //            jQuery('#size').remove();
+ //            jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
+ //        });
 
         jQuery('.about .image_container, .text_container.left, .top_post, .tool.left').viewportChecker({
 
               classToAdd: 'animated fadeInLeft',
-              // repeat: true,
+       
 
         });
 
         jQuery('.about .text, .text_container.right, .single_post, .tool.right').viewportChecker({
 
               classToAdd: 'animated fadeInRight',
-              // repeat: true,
+              
 
         });
 
-        jQuery('.service, .portfolio_container, .single_member_inner_container, .stat_container>*,.how_we_work .image_container, .single_package>*, .single_feature>*, .member_details').viewportChecker({
+        jQuery('.service, .portfolio_container, .single_member_inner_container,.how_we_work .image_container, .single_package>*, .single_feature>*, .member_details').viewportChecker({
 
               classToAdd: 'animated zoomIn',
-              // repeat: true,
+            
 
         });
 
         jQuery('.call , .content>.my_btn, .tool.bottom, .button_container').viewportChecker({
 
               classToAdd: 'animated fadeInUp',
-              // repeat: true,
         });
 
+    jQuery('.stat_container>*').viewportChecker({
+        classToAdd: 'animated zoomIn', 
+
+        callbackFunction: function(elem, action){
+
+            elem.closest('.stat_counter').addClass('active').spincrement({
+
+              from: 0,                
+              to: null,              
+              decimalPlaces: 0,       
+              duration: 3000 ,
+              leeway: 10,
+                  complete: function (e) {
+                  e.text( e.text() + " +")
+                  }
+
+            });
+
+        }
+
+    });
+
+    /* Responsive meni */
 
 
-	
+    jQuery(".navbar-toggle").click(function(event) {
+        /* Act on the event */
+
+            if (jQuery(this).hasClass('collapsed')) {
+
+            jQuery(".navbar-default").addClass('js_response_meni').next(".main_container").addClass('js_response_hide');
+            } 
+
+            else {
+
+            jQuery(".navbar-default").removeClass('js_response_meni').next(".main_container").removeClass('js_response_hide');
+            }
+
+        });
+
 });
